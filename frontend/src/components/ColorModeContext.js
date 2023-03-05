91e70d4db677142b949f5bb00108dc0b9ae32bd2
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-05 15:00:04 +0000

    Code refactoring

--- frontend/src/components/ColorModeContext.js
+++ frontend/src/components/ColorModeContext.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { createContext } from 'react';
 
-const ColorModeContext = React.createContext({
-    toggleColorMode: () => {}
+const ColorModeContext = createContext({
+  toggleColorMode: () => {}
 });
 
 export default ColorModeContext;
