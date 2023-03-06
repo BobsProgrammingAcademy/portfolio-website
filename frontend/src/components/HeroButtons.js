1b3ce4cd6b27fd0616d91b1c32c232988ee8bdd4
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-06 22:01:58 +0000

    Code refactoring

--- frontend/src/components/HeroButtons.js
+++ frontend/src/components/HeroButtons.js
@@ -7,78 +7,78 @@ import HelpIcon from '@mui/icons-material/HelpOutlineOutlined';
 import ManageSearchIcon from '@mui/icons-material/ManageSearchOutlined';
 
 const HeroButtons = () => {
-    const theme = useTheme();
-    const isMd = useMediaQuery(
-        theme.breakpoints.up('md'),
-        { defaultMatches: true }
-    );
+  const theme = useTheme();
+  const isMd = useMediaQuery(
+    theme.breakpoints.up('md'),
+    { defaultMatches: true }
+  );
     
-    return (
-        <React.Fragment>
-            <Box
-                display='flex'
-                flexDirection={{ xs: 'column', sm: 'row' }}
-                alignItems={{ xs: 'stretched', sm: 'flex-start' }}
-                justifyContent='center'
-                marginTop={4}
-            >
-                <Button
-                    component='a'
-                    variant='contained'
-                    color='primary'
-                    size='large'
-                    href='#about'
-                    endIcon={<HelpIcon />}
-                    fullWidth={isMd ? false : true}
-                    disableElevation={true}
-                    sx={{
-                        padding: '15px 30px',
-                        marginRight: '15px',
-                        fontSize: '16px',
-                        textTransform: 'uppercase',
-                        border: '2px solid ' + theme.palette.primary.main,
-                        '&:hover': {
-                            backgroundColor: 'transparent',
-                            color: theme.palette.primary.main,
-                            border: '2px solid ' + theme.palette.primary.main
-                        }
-                    }}
-                >
-                    Learn More
-                </Button>
-                <Box
-                    marginTop={{ xs: 2, sm: 0 }}
-                    marginLeft={{ sm: 1 }}
-                    width={{ xs: '100%', md: 'auto' }}
-                >
-                    <Button
-                        component='a'
-                        variant='outlined'
-                        color='primary'
-                        size='large'
-                        href='#'
-                        endIcon={<ManageSearchIcon />}
-                        fullWidth={isMd ? false : true}
-                        disableElevation={true}
-                        sx={{
-                            padding: '15px 30px',
-                            marginRight: '15px',
-                            fontSize: '16px',
-                            textTransform: 'uppercase',
-                            border: '2px solid ' + theme.palette.primary.main,
-                            '&:hover': {
-                                backgroundColor: theme.palette.primary.main,
-                                color: theme.palette.common.white,
-                                border: '2px solid ' + theme.palette.primary.main
-                            }
-                        }}
-                    >
-                        View Our Work
-                    </Button>
-                </Box>
-            </Box>
-        </React.Fragment>
-    );
+  return (
+    <>
+      <Box
+        display='flex'
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        alignItems={{ xs: 'stretched', sm: 'flex-start' }}
+        justifyContent='center'
+        marginTop={4}
+      >
+        <Button
+          component='a'
+          variant='contained'
+          color='primary'
+          size='large'
+          href='#about'
+          endIcon={<HelpIcon />}
+          fullWidth={isMd ? false : true}
+          disableElevation={true}
+          sx={{
+            padding: '15px 30px',
+            marginRight: '15px',
+            fontSize: '16px',
+            textTransform: 'uppercase',
+            border: '2px solid ' + theme.palette.primary.main,
+            '&:hover': {
+              backgroundColor: 'transparent',
+              color: theme.palette.primary.main,
+              border: '2px solid ' + theme.palette.primary.main
+            }
+          }}
+        >
+          Learn More
+        </Button>
+        <Box
+          marginTop={{ xs: 2, sm: 0 }}
+          marginLeft={{ sm: 1 }}
+          width={{ xs: '100%', md: 'auto' }}
+        >
+          <Button
+            component='a'
+            variant='outlined'
+            color='primary'
+            size='large'
+            href='#'
+            endIcon={<ManageSearchIcon />}
+            fullWidth={isMd ? false : true}
+            disableElevation={true}
+            sx={{
+              padding: '15px 30px',
+              marginRight: '15px',
+              fontSize: '16px',
+              textTransform: 'uppercase',
+              border: '2px solid ' + theme.palette.primary.main,
+              '&:hover': {
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.common.white,
+                border: '2px solid ' + theme.palette.primary.main
+              }
+            }}
+          >
+            View Our Work
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default HeroButtons;
