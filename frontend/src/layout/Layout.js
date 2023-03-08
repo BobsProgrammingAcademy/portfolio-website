387985796419e0af06fb0aff8731aee98f2197ef
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-03-08 23:08:58 +0000

    Code refactoring

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
@@ -14,80 +14,80 @@ import Footer from './Footer';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
-    const theme = useTheme();
-    const isLg = useMediaQuery(
-        theme.breakpoints.up('lg'),
-        { defaultMatches: true }
-    );
+  const theme = useTheme();
+  const isLg = useMediaQuery(
+    theme.breakpoints.up('lg'),
+    { defaultMatches: true }
+  );
 
-    const [openSidebar, setOpenSidebar] = React.useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
     
-    const handleSidebarOpen = () => {
-        setOpenSidebar(true);
-    };
+  const handleSidebarOpen = () => {
+    setOpenSidebar(true);
+  };
     
-    const handleSidebarClose = () => {
-        setOpenSidebar(false);
-    };
+  const handleSidebarClose = () => {
+    setOpenSidebar(false);
+  };
     
-    const open = isLg ? false : openSidebar;
+  const open = isLg ? false : openSidebar;
 
-    const trigger = useScrollTrigger({
-        disableHysteresis: true,
-        threshold: 100,
-      });
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
     
-    const scrollTo = (id) => {
-        setTimeout(() => {
-            const element = document.querySelector(`#${id}`);
-            if (!element) {
-                return;
-            }
-            window.scrollTo({ left: 0, top: element.offsetTop, behavior: 'smooth' });
-        });
-    };
+  const scrollTo = (id) => {
+    setTimeout(() => {
+      const element = document.querySelector(`#${id}`);
+      if (!element) {
+        return;
+      }
+      window.scrollTo({ left: 0, top: element.offsetTop, behavior: 'smooth' });
+    });
+  };
 
-    return (
-        <Box id='page-top'>
-            <Header onSidebarOpen={handleSidebarOpen} />
-            <Sidebar
-                onClose={handleSidebarClose}
-                open={open}
-            />
-            <Box>
-                {children}
-            </Box>
-            <Footer />
-            <NoSsr>
-                <Zoom in={trigger}>
-                    <Box
-                        onClick={() => scrollTo('page-top')}
-                        role='presentation'
-                        sx={{ position: 'fixed', bottom: 24, right: 32 }}
-                    >
-                        <Fab 
-                            color='primary' 
-                            size='small' 
-                            aria-label='scroll back to top'
-                            sx={{
-                                '&:hover': {
-                                    backgroundColor: 'transparent',
-                                    color: theme.palette.primary.main,
-                                    border: '2px solid' + theme.palette.primary.main,
-                                },
-                            }}
-                        >
-                            <KeyboardArrowUpIcon />
-                        </Fab>
-                    </Box>
-                </Zoom>
-            </NoSsr>
-        </Box>
-    );
+  return (
+    <Box id='page-top'>
+      <Header onSidebarOpen={handleSidebarOpen} />
+      <Sidebar
+        onClose={handleSidebarClose}
+        open={open}
+      />
+      <Box>
+        {children}
+      </Box>
+      <Footer />
+      <NoSsr>
+        <Zoom in={trigger}>
+          <Box
+            onClick={() => scrollTo('page-top')}
+            role='presentation'
+            sx={{ position: 'fixed', bottom: 24, right: 32 }}
+          >
+            <Fab 
+              color='primary' 
+              size='small' 
+              aria-label='scroll back to top'
+              sx={{
+                '&:hover': {
+                  backgroundColor: 'transparent',
+                  color: theme.palette.primary.main,
+                  border: '2px solid' + theme.palette.primary.main,
+                },
+              }}
+            >
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </Box>
+        </Zoom>
+      </NoSsr>
+    </Box>
+  );
 };
 
 Layout.propTypes = {
-    children: PropTypes.node
+  children: PropTypes.node
 };
 
 export default Layout;
