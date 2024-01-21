c5b3d19e31d830be9b0915fbdecf635964cb8c79
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-01-21 19:47:52 +0100

    Code refactoring

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -29,7 +29,7 @@ const Projects = () => {
       .then((response) => {
         setProjects(response.data);
       })
-      .catch((err) => console.log(err));
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
