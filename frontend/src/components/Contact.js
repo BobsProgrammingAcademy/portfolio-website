df322f2edda30cff2831a28b080979043be4baa0
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-01-21 19:47:14 +0100

    Code refactoring

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -27,7 +27,7 @@ const Contact = () => {
       .then((response) => {
         setContact(response.data);
       })
-      .catch((err) => console.log(err));
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
