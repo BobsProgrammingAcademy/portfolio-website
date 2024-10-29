14a58337f3d7c30fdc66d2ba45b2d57260914de0
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-10-29 06:01:57 +0100

    Code refactoring

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 
 # MEDIA_ROOT is for the user-uploaded content
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
