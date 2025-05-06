96b6259ed18a4d66c6a42f48e2aeeb3f578f6c1d
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-05-06 20:04:49 +0200

    Code refactoring

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+// Material UI
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
